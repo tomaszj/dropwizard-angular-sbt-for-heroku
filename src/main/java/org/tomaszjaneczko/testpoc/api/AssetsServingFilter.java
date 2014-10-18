4fdb3545628626ec0cd69cd9e3cb56dedb79f10e
Tomasz Janeczko <t.janeczko@kainos.com>
2014-10-18 19:48:32 +0100

    Added comments to AssetsServingFilter class.

--- src/main/java/org/tomaszjaneczko/testpoc/api/AssetsServingFilter.java
+++ src/main/java/org/tomaszjaneczko/testpoc/api/AssetsServingFilter.java
@@ -8,23 +8,35 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Filter used to render main index.html in servlet set up by AssetsBundle.
+ *
+ * All requests that don't fetch .js, .css or images, go to the same file,
+ * which is the entry point to single-page applications.
+ *
+ * Assumption here is that assets servlet is set up correctly and already
+ * lets through all API requests to respective Resources.
+ *
+ * Filter is set up in Dropwizard's Application subclass.
+ */
 public class AssetsServingFilter implements Filter {
 
+    /**
+     * List of prefixes that should be let through.
+     */
     public static final List<String> ALLOWED_PREFIXES = Lists.newArrayList(
         "/js/",
         "/css/",
         "/img/"
     );
 
+    /**
+     * List of full paths that should be let through, including the root.
+     */
     public static final List<String> ALLOWED_PATHS = Lists.newArrayList(
             "/favicon.ico",
             "/"
     );
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        //
-    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -34,14 +46,16 @@ public class AssetsServingFilter implements Filter {
         if (matchesAllowedPaths(request.getRequestURI())) {
             filterChain.doFilter(request, response);
         } else {
-            request.getRequestDispatcher("/").forward(request, servletResponse);
+            // Request should be forwarded to the root.
+            request.getRequestDispatcher("/").forward(request, response);
         }
     }
 
     @Override
-    public void destroy() {
-        //
-    }
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy() {}
 
     public boolean matchesAllowedPaths(String path) {
          return ALLOWED_PREFIXES.stream().anyMatch(path::startsWith)
